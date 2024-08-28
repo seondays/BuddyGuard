9288cd20a21fadd971a14e262f6fc22679f6452f
shimbaa <shim9597@gmail.com>
2024-08-28 12:37:53 +0900

    init

---
+++ be/src/main/java/buddyguard/mybuddyguard/MyBuddyGuardApplication.java
@@ -0,0 +1,13 @@
+package buddyguard.mybuddyguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBuddyGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBuddyGuardApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/buddyguard/mybuddyguard/MyBuddyGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package buddyguard.mybuddyguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBuddyGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
