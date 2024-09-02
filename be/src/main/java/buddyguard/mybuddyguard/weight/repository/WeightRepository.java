98fffca92de9168291c2941b2daca5c074b4f558
shimbaa <shim9597@gmail.com>
2024-09-02 23:40:46 +0900

    feat: 체중 repository 구현 #8

---
+++ be/src/main/java/buddyguard/mybuddyguard/weight/repository/WeightRepository.java
@@ -0,0 +1,8 @@
+package buddyguard.mybuddyguard.weight.repository;
+
+import buddyguard.mybuddyguard.weight.domain.Weight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeightRepository extends JpaRepository<Weight, Long> {
+
+}
