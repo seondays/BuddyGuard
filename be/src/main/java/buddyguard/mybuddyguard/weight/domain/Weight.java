034da8197f7d92b872e7bd741cfdecc836c771f6
shimbaa <shim9597@gmail.com>
2024-09-02 23:39:41 +0900

    feat: Weight(체중) 엔티티 구현 #8

---
+++ be/src/main/java/buddyguard/mybuddyguard/weight/domain/Weight.java
@@ -0,0 +1,36 @@
+package buddyguard.mybuddyguard.weight.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "WEIGHT_RECORDS")
+@ToString
+public class Weight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    long petId;
+
+    LocalDateTime recordedAt;
+
+    double weight;
+
+    @Builder
+    public Weight(long petId, LocalDateTime recordedAt, double weight) {
+        this.petId = petId;
+        this.recordedAt = recordedAt;
+        this.weight = weight;
+    }
+}
