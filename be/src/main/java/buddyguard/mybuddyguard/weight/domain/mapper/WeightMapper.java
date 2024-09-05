b55b408877d74e1c106a9361b15d64321835e176
shimbaa <shim9597@gmail.com>
2024-09-05 22:35:30 +0900

    feat: dto 와 entity 간의 mapping 기능을 분리한 유틸 클래스 구현 #8

---
+++ be/src/main/java/buddyguard/mybuddyguard/weight/domain/mapper/WeightMapper.java
@@ -0,0 +1,31 @@
+package buddyguard.mybuddyguard.weight.domain.mapper;
+
+import buddyguard.mybuddyguard.weight.domain.Weight;
+import buddyguard.mybuddyguard.weight.dto.WeightCreateRequest;
+import buddyguard.mybuddyguard.weight.dto.WeightResponse;
+import java.util.List;
+
+public class WeightMapper {
+
+    public static WeightResponse toResponse(Weight weight) {
+        return new WeightResponse(
+                weight.getId(),
+                weight.getPetId(),
+                weight.getWeight(),
+                weight.getRecordedAt());
+    }
+
+    public static List<WeightResponse> toResponseList(List<Weight> weights) {
+        return weights.stream()
+                .map(WeightMapper::toResponse)
+                .toList();
+    }
+
+    public static Weight toEntity(WeightCreateRequest request) {
+        return Weight.builder()
+                .petId(request.petId())
+                .recordedAt(request.recordedAt())
+                .weight(request.weight())
+                .build();
+    }
+}
