d772b7b9cc6634aae0fa558d6f8d3c16b37b268a
seondays <agseon9@gmail.com>
2024-09-07 17:10:23 +0900

    feat : 로그인 로직을 위한 entity, service, repository 구현 #3
    로그인 한 유저를 확인하고, 최초 로그인 회원이면 DB에 저장하는 기능을 담당하는 service 객체와 DB 저장을 위한 엔티티, 레포지토리 구현

---
+++ be/src/main/java/buddyguard/mybuddyguard/login/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package buddyguard.mybuddyguard.login.repository;
+
+import buddyguard.mybuddyguard.login.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Users findByOauthId(String oAuthId);
+}
