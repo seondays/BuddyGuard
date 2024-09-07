43962b546a324c77c41b746879125c733a9fe089
seondays <agseon9@gmail.com>
2024-09-07 17:47:34 +0900

    feat : 로그인 처리를 위한 OAuth2User 역할 객체 커스텀 구현 #3
    로그인 처리를 위해 provider가 사용할 OAuth2User 객체 구현

---
+++ be/src/main/java/buddyguard/mybuddyguard/login/dto/CustomOAuth2User.java
@@ -0,0 +1,40 @@
+package buddyguard.mybuddyguard.login.dto;
+
+import buddyguard.mybuddyguard.login.controller.request.OAuth2Request;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class CustomOAuth2User implements OAuth2User {
+    private final OAuth2Request oAuth2Request;
+    private final String role;
+
+    public CustomOAuth2User(OAuth2Request oAuth2Request, String role) {
+        this.oAuth2Request = oAuth2Request;
+        this.role = role;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return role;
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getName() {
+        return oAuth2Request.getName();
+    }
+}
