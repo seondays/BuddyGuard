ab69180bef9a30e8b4ce83386ea6b38b79160649
jinjaehwan <jjh099@icloud.com>
2024-09-07 17:05:30 +0900

    feat: Button 컴포넌트 추가
    components의 atoms에 Button 컴포넌트 추가
    
    Related to : #12

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,29 +1,27 @@
-import { useState } from 'react';
-
-import reactLogo from '@public/react.svg';
-import viteLogo from '@public/vite.svg';
+import  { useState } from 'react';
+import Button from './components/atoms/Button';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsButtonClicked((prev) => !prev);  
+  };
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div style={{ width: "11rem" }}>
+      <Button>
+        기본
+      </Button>
+      <Button 
+        width="11rem" 
+        height="4rem" 
+        isClicked={isButtonClicked}  
+        onClick={handleButtonClick}  
+      >
+        클릭 상태 변경 버튼
+      </Button>
+    </div>
   );
 }
 
---
+++ fe/src/components/atoms/Button.tsx
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  width?: string;
+  height?: string;
+  bgColor?: string;
+  border?: string;
+  fontSize?: string;
+  display?: string;
+  textAlign?: string;
+  justifyContent?: string;
+  alignItems?: string;
+  isClicked?: boolean;
+  onClick?: () => void;
+  children: React.ReactNode;
+}
+
+const Button = ({
+  children,
+  width = '100%',
+  height = '4rem',
+  bgColor = 'white',
+  border = `${theme.colorValues.special.textForce02} solid 0.1rem`,
+  display = 'flex',
+  fontSize = '1rem',
+  textAlign = 'center',
+  justifyContent = 'center',
+  alignItems = 'center',
+  isClicked = false,
+  onClick,
+  ...rest
+}: ButtonProps) => (
+  <StyledButton
+    width={width}
+    height={height}
+    bgColor={bgColor}
+    border={border}
+    display={display}
+    fontSize={fontSize}
+    textAlign={textAlign}
+    alignItems={alignItems}
+    isClicked={isClicked}
+    onClick={onClick}
+    {...rest}
+  >
+    {children}
+  </StyledButton>
+);
+
+const getButtonStyle = (isClicked: boolean, bgColor: string | undefined) => ({
+  backgroundColor: isClicked ? theme.colorValues.special.modalBg : bgColor,
+  boxShadow: isClicked ? '0px 4px 12px rgba(0, 0, 0, 0.2)' : 'none',
+  transform: isClicked ? 'translateY(-2px)' : 'none',
+});
+
+const StyledButton = styled.button<ButtonProps & { isClicked: boolean }>`
+  display: ${({ display }) => display};
+  justify-content: ${({ justifyContent }) => justifyContent};
+  align-items: ${({ alignItems }) => alignItems};
+  text-align: ${({ textAlign }) => textAlign};
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  ${({ isClicked, bgColor }) => getButtonStyle(isClicked, bgColor)};  
+  border: ${({ border }) => border};
+  font-size: ${({ fontSize }) => fontSize};
+  color: ${({ color = theme.colorValues.grayscale[900] }) => color};
+  cursor: pointer;
+  transition: all 0.3s ease;
+  &:hover, &:focus {
+    background-color: ${theme.colorValues.special.modalBg};
+  }
+`;
+
+export default Button;
