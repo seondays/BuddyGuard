ba3fa36b6a4942a406a8ff2f97737e42e9648b4c
minjeongHEO <hmjoung9770@gmail.com>
2024-09-09 19:33:42 +0900

    feat: 체크박스 체크 아이콘 추가 및 사이즈 적용

--- fe/src/components/atoms/Checkbox.tsx
+++ fe/src/components/atoms/Checkbox.tsx
@@ -1,71 +1,85 @@
 import { InputHTMLAttributes, useState } from 'react';
 import styled from 'styled-components';
 
-import CheckIcon from '@public/assets/icons/CheckIcon.svg';
+import CheckIcon from '@/components/icons/CheckIcon';
 
-type checkboxSizeType = 'small' | 'medium' | 'large';
+type CheckboxSizeType = 'small' | 'medium' | 'large';
 
-type CheckboxSizeMap = {
-  [key in checkboxSizeType]: string;
-};
-
-const checkboxSize: CheckboxSizeMap = {
+const checkboxSize: { [key in CheckboxSizeType]: string } = {
   small: '1.25rem',
   medium: '1.5rem',
   large: '2rem',
 };
 
-export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
-  checkboxSize?: checkboxSizeType;
+const checkIconSize: { [key in CheckboxSizeType]: number } = {
+  small: 13,
+  medium: 17,
+  large: 25,
+};
+
+export interface CheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  size?: CheckboxSizeType;
   label?: string;
   isChecked?: boolean;
 }
 
-export default function Checkbox({ checkboxSize = 'small', label = '테스트', isChecked = false }: CheckboxProps) {
+export default function Checkbox({ size = 'small', label = '', isChecked = false }: CheckboxProps) {
   const [checked, setChecked] = useState(isChecked);
 
   const handleCheckboxChange = () => setChecked((prevChecked) => !prevChecked);
 
   return (
-    <CheckboxContainer>
-      <HiddenCheckbox id="chk" checked={checked} onChange={handleCheckboxChange} />
-      <StyledCheckbox htmlFor="chk" checked={checked} $size={checkboxSize} />
-      <CheckboxLabel htmlFor="chk">{label}</CheckboxLabel>
-    </CheckboxContainer>
+    <StyledCheckboxContainer>
+      <StyledCheckboxLabel htmlFor="chk" size={size}>
+        {label}
+      </StyledCheckboxLabel>
+      <StyledHiddenCheckbox id="chk" checked={checked} onChange={handleCheckboxChange} />
+      <StyledCheckbox htmlFor="chk" checked={checked} size={size}>
+        {checked && <CheckIcon size={checkIconSize[size]} />}
+      </StyledCheckbox>
+    </StyledCheckboxContainer>
   );
 }
 
-const CheckboxContainer = styled.div`
-  display: inline-block;
+const StyledCheckboxContainer = styled.div`
+  display: inline-flex;
+  align-items: center;
   vertical-align: middle;
 `;
 
-const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
-  display: none; /* 체크박스 숨기기 */
-`;
-
-const StyledCheckbox = styled.label<{ checked: boolean; $size: checkboxSizeType }>`
-  width: ${({ $size }) => checkboxSize[$size]};
-  height: ${({ $size }) => checkboxSize[$size]};
-  border: ${({ theme }) => `0.1rem solid ${theme.colorValues.grayscale[200]}`};
-  display: inline-block;
-  cursor: pointer;
-  border-radius: ${({ $size, theme }) => theme.radius[$size]};
-  user-select: none; /* 드래그 금지 */
-  background: ${({ checked, theme }) => (checked ? theme.colorValues.special.textForce : 'transparent')};
-
-  &::after {
-    content: ${({ checked }) => (checked ? CheckIcon : '""')};
-    color: white; // 선택된 아이콘 색상
-    font-size: ${({ $size }) => checkboxSize[$size]};
-  }
-`;
-
-const CheckboxLabel = styled.label`
-  font-size: 30px;
-  position: relative;
-  top: 6px;
-  letter-spacing: -5px;
+const StyledCheckboxLabel = styled.label<{ size: CheckboxSizeType }>`
+  font-size: ${({ theme, size }) => theme.typography[size]};
+  margin-right: ${({ theme, size }) => theme.spacing[size]};
   cursor: pointer;
   user-select: none;
+
+  /* TODO: 다크모드 color:  */
+`;
+
+const StyledHiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+`;
+
+const StyledCheckbox = styled.label<{ checked: boolean; size: CheckboxSizeType }>`
+  width: ${({ size }) => checkboxSize[size]};
+  height: ${({ size }) => checkboxSize[size]};
+  border: ${({ theme }) => `0.1rem solid ${theme.colorValues.grayscale[200]}`};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border-radius: ${({ size, theme }) => theme.radius[size]};
+  user-select: none;
+  background: ${({ checked, theme }) =>
+    checked ? theme.colorValues.special.textForce : theme.colorValues.grayscale[50]};
+  transition: all 0.2s ease-in-out;
+
+  svg {
+    width: 80%;
+    height: 80%;
+  }
 `;
