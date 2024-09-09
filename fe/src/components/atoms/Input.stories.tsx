95f572cd08c5317d139154790f4a05a2da43e2dd
minjeongHEO <hmjoung9770@gmail.com>
2024-09-09 15:00:55 +0900

    feat: input 컴포넌트 및 스토리북 작성 중 Related to: [FEAT] 공통 컴포넌트 생성 #12

--- fe/src/components/atoms/Input.stories.tsx
+++ fe/src/components/atoms/Input.stories.tsx
@@ -6,17 +6,17 @@ export default {
   title: 'Components/Input',
   component: Input,
   argTypes: {
-    label: {
+    default: {
       description: 'Input field의 라벨 텍스트',
       control: 'text',
     },
-    fullWidth: {
-      description: 'Input이 가로 전체를 차지할지 여부',
+    bottomLine: {
+      description: '하단 라인 존재 여부',
       control: 'boolean',
     },
-    hasError: {
-      description: 'Input에 에러가 있는지 여부',
-      control: 'boolean',
+    customWidth: {
+      description: 'Input width 조정',
+      control: 'number',
     },
   },
 } as Meta<InputProps>;
@@ -26,23 +26,26 @@ const Template: StoryFn<InputProps> = (args) => <Input {...args} />;
 // 기본 Input 스토리
 export const Default = Template.bind({});
 Default.args = {
-  label: 'Username', // 기본으로 보여질 label 값
-  fullWidth: false, // 기본은 전체 너비가 아님
-  hasError: false, // 기본 에러 없음
+  label: '제목', // 기본으로 보여질 label 값
+  $placeholder: '제목을 입력하세요.',
+  widthPercent: 100, // 기본 100%
+  isBottomLine: true, // 기본 하단 라인 있음
 };
 
-// 전체 너비를 차지하는 Input 스토리
-export const FullWidth = Template.bind({});
-FullWidth.args = {
-  label: 'Email',
-  fullWidth: true, // 전체 너비 사용
-  hasError: false,
+// 하단 라인 없는 스토리
+export const BottomLine = Template.bind({});
+Default.args = {
+  label: '제목', // 기본으로 보여질 label 값
+  $placeholder: '제목을 입력하세요.',
+  widthPercent: 100, // 기본 100%
+  isBottomLine: false, // 기본 하단 라인 있음
 };
 
-// 에러 상태를 가진 Input 스토리
-export const WithError = Template.bind({});
-WithError.args = {
-  label: 'Password',
-  fullWidth: false,
-  hasError: true, // 에러 상태
+// 너비 지정한 Input 스토리
+export const CustomWidth = Template.bind({});
+CustomWidth.args = {
+  label: '제목', // 기본으로 보여질 label 값
+  $placeholder: '제목을 입력하세요.',
+  widthPercent: 50, // 기본 100%
+  isBottomLine: true, // 기본 하단 라인 있음
 };
--- fe/src/components/atoms/Input.tsx
+++ fe/src/components/atoms/Input.tsx
@@ -1,51 +1,75 @@
 import { InputHTMLAttributes } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import theme from '@/styles/theme';
 
-const {
-  special: { textForce },
-} = theme.colorValues;
-
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
-  fullWidth?: boolean;
-  hasError?: boolean;
+  $placeholder: string;
+  widthPercent?: number;
+  isBottomLine: boolean;
 }
 
-export default function Input({ label, fullWidth, hasError, ...rest }: InputProps) {
+export default function Input({
+  label,
+  $placeholder = '',
+  widthPercent = 100,
+  isBottomLine = true,
+  ...rest
+}: InputProps) {
   return (
-    <InputWrapper fullWidth={fullWidth}>
+    <InputWrapper widthPercent={widthPercent} isBottomLine={isBottomLine}>
       {label && <StyledLabel>{label}</StyledLabel>}
-      <StyledInput hasError={hasError} {...rest} />
+      <StyledInput placeholder={$placeholder} {...rest} />
     </InputWrapper>
   );
 }
 
-const InputWrapper = styled.div<{ fullWidth?: boolean }>`
+const InputWrapper = styled.div<{ widthPercent: number; isBottomLine: boolean }>`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
   margin-bottom: 1rem;
-  ${({ fullWidth }) =>
-    fullWidth &&
-    css`
+  padding: 0.3rem 1rem;
+  width: ${({ widthPercent }) => `${widthPercent}%`};
+  position: relative;
+
+  // props를 통해 테마에 접근하여 border-bottom 스타일 설정 : 이건 되는데
+  /* border-bottom: 2px solid ${({ theme }) => theme.colorValues.grayscale[100]}; */
+
+  // props를 통해 테마에 접근하여 조건부 ::after 스타일 설정 : 이거 에러남
+  ${({ isBottomLine, theme }) =>
+    isBottomLine &&
+    `
+    &:after {
+      content: '';
+      position: absolute;
       width: 100%;
-    `}
+      height: 2px;
+      left: 0;
+      bottom: 0;
+      background-color: ${theme.colorValues.grayscale[100]};
+    }
+   `}
 `;
 
 const StyledLabel = styled.label`
-  margin-bottom: 0.5rem;
-  font-weight: bold;
+  margin-right: 0.5rem;
+  min-width: 4rem;
   color: ${theme.lightTheme.textSecondary};
 `;
 
 const StyledInput = styled.input<{ hasError?: boolean }>`
-  caret-color: ${textForce};
+  /* background-color: red; */
+  width: 100%;
+  caret-color: ${theme.colorValues.special.textForce};
   padding: 0.5rem;
   font-size: 1rem;
-  ${({ hasError }) =>
-    hasError &&
-    css`
-      background-color: red;
-    `}
+  border: none;
+  outline: none;
+
+  & focus {
+    outline: none;
+  }
 `;
