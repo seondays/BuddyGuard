8dd6dd985f80a98a7ab125e477f582703e2f0f63
minjeongHEO <hmjoung9770@gmail.com>
2024-09-10 17:58:14 +0900

    feat: 루트에 프로바이더 적용 #13

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,15 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 
 import App from './App.tsx';
-import theme from './styles/theme.ts';
-
-import './index.css';
+import ThemeProvider from './context/ThemeProvider.tsx';
+import { GlobalStyles } from './styles/globalStyles.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider>
+      <GlobalStyles />
       <App />
     </ThemeProvider>
   </StrictMode>
