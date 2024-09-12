2214187ed707bd8b9df6c9467c1789eff219b605
minjeongHEO <hmjoung9770@gmail.com>
2024-09-12 22:05:02 +0900

    feat: reset.css 적용

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,10 +2,12 @@ import { RouterProvider } from 'react-router-dom';
 
 import { router } from './routes/router';
 import { GlobalStyles } from './styles/globalStyles';
+import { ResetStyles } from './styles/resetStyles';
 
 function App() {
   return (
     <>
+      <ResetStyles />
       <GlobalStyles />
       <RouterProvider router={router} />
     </>
