9cf8c3bf30d4aaa0cdad2b2c85f98d7dfa1979a6
minjeongHEO <hmjoung9770@gmail.com>
2024-09-13 17:14:24 +0900

    refactor: 이미지 컴포넌트 prop 네이밍 경고 수정

--- fe/src/components/atoms/Image.tsx
+++ fe/src/components/atoms/Image.tsx
@@ -3,17 +3,17 @@ import styled from 'styled-components';
 export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   width?: string;
   height?: string;
-  borderRadius?: string;
-  boxShadow?: boolean;
-  isClicked?: boolean;
+  $borderRadius?: string;
+  $boxShadow?: boolean;
+  $isClicked?: boolean;
   src: string;
 }
 export default function Image({
   width = '100%',
   height = 'auto',
-  borderRadius = '0',
-  boxShadow = false,
-  isClicked = false,
+  $borderRadius = '0',
+  $boxShadow = false,
+  $isClicked = false,
   src,
   ...rest
 }: ImageProps) {
@@ -21,25 +21,26 @@ export default function Image({
     <StyledImage
       width={width}
       height={height}
-      borderRadius={borderRadius}
-      boxShadow={boxShadow}
-      isClicked={isClicked}
+      $borderRadius={$borderRadius}
+      $boxShadow={$boxShadow}
+      $isClicked={$isClicked}
       src={src}
       {...rest}
     />
   );
 }
 
-const StyledImage = styled.img<ImageProps & { isClicked: boolean }>`
+const StyledImage = styled.img<ImageProps & { $isClicked: boolean }>`
   width: ${({ width }) => width};
   height: ${({ height }) => height};
-  border-radius: ${({ borderRadius }) => borderRadius};
+  border-radius: ${({ $borderRadius }) => $borderRadius};
   background: transparent;
   cursor: pointer;
 
   // 클릭 상태와 박스 쉐도우 여부에 따라 스타일 동적 적용
-  box-shadow: ${({ isClicked, boxShadow }) => (isClicked || boxShadow ? '0px 4px 12px rgba(0, 0, 0, 0.2)' : 'none')};
-  transform: ${({ isClicked }) => (isClicked ? 'scale(1.05)' : 'none')};
+  box-shadow: ${({ $isClicked, $boxShadow }) =>
+    $isClicked || $boxShadow ? '0px 4px 12px rgba(0, 0, 0, 0.2)' : 'none'};
+  transform: ${({ $isClicked }) => ($isClicked ? 'scale(1.05)' : 'none')};
 
   transition: all 0.3s ease;
 
