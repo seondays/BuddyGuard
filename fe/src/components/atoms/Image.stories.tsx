234efa29bc0fc5a931491c9ec449254af9fdac55
jinjaehwan <jjh099@icloud.com>
2024-09-16 02:30:30 +0900

    refactor: Image 스토리북 수정

--- fe/src/components/atoms/Image.stories.tsx
+++ fe/src/components/atoms/Image.stories.tsx
@@ -1,19 +1,19 @@
 import { Meta, StoryFn } from '@storybook/react';
-
-import CalendarIcon from '@public/assets/icons/calendarIcon.png';
-
 import Image, { ImageProps } from './Image';
 
 export default {
   title: 'Atoms/Image',
   component: Image,
   argTypes: {
-    width: { control: 'text' },
-    height: { control: 'text' },
     $borderRadius: { control: 'text' },
     $boxShadow: { control: 'boolean' },
     $isClicked: { control: 'boolean' },
     src: { control: 'text' },
+    text: { control: 'text' },
+    textPosition: {
+      control: { type: 'select', options: ['top', 'bottom', 'left', 'right'] },
+    },
+    style: { control: 'object' },
   },
 } as Meta<ImageProps>;
 
@@ -22,43 +22,59 @@ const Template: StoryFn<ImageProps> = (args) => <Image {...args} />;
 // 기본 이미지 스토리
 export const Default = Template.bind({});
 Default.args = {
-  src: CalendarIcon,
-  width: '10%',
-  height: 'auto',
-  $borderRadius: '0',
+  src: 'https://via.placeholder.com/300',
+  $borderRadius: '8px',
   $boxShadow: false,
   $isClicked: false,
+  text: '',
+  textPosition: 'bottom',
+  style: {
+    width: '300px',
+    height: 'auto',
+  },
 };
 
-// 이미지 클릭된 상태 스토리
-export const Clicked = Template.bind({});
-Clicked.args = {
-  src: CalendarIcon,
-  width: '10%',
-  height: 'auto',
-  $borderRadius: '0',
+// 텍스트가 포함된 이미지
+export const ImageWithText = Template.bind({});
+ImageWithText.args = {
+  src: 'https://via.placeholder.com/300',
+  $borderRadius: '8px',
+  $boxShadow: false,
+  $isClicked: false,
+  text: 'Sample Text',
+  textPosition: 'bottom',
+  style: {
+    width: '300px',
+    height: 'auto',
+  },
+};
+
+// 클릭된 상태의 이미지
+export const ClickedImage = Template.bind({});
+ClickedImage.args = {
+  src: 'https://via.placeholder.com/300',
+  $borderRadius: '8px',
   $boxShadow: true,
   $isClicked: true,
+  text: 'Clicked Image',
+  textPosition: 'bottom',
+  style: {
+    width: '300px',
+    height: 'auto',
+  },
 };
 
-// 박스 쉐도우가 있는 이미지 스토리
-export const WithBoxShadow = Template.bind({});
-WithBoxShadow.args = {
-  src: CalendarIcon,
-  width: '10%',
-  height: 'auto',
-  $borderRadius: '0.5rem',
+// 좌측에 텍스트가 있는 이미지
+export const LeftTextImage = Template.bind({});
+LeftTextImage.args = {
+  src: 'https://via.placeholder.com/300',
+  $borderRadius: '8px',
   $boxShadow: true,
   $isClicked: false,
-};
-
-// 둥근 모서리를 가진 이미지 스토리
-export const RoundedImage = Template.bind({});
-RoundedImage.args = {
-  src: CalendarIcon,
-  width: '10%',
-  height: 'auto',
-  $borderRadius: '50%',
-  $boxShadow: true,
-  $isClicked: false,
+  text: 'Text on Left',
+  textPosition: 'left',
+  style: {
+    width: '300px',
+    height: 'auto',
+  },
 };
