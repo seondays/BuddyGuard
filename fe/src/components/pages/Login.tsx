03c0a788cbfdeabf903785d515b2b3c2f3420c2e
jinjaehwan <jjh099@icloud.com>
2024-09-17 17:31:35 +0900

    feat: 로그인 UI 추가  Please enter the commit message for your changes. Lines starting

--- fe/src/components/pages/Login.tsx
+++ fe/src/components/pages/Login.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import Image from '../atoms/Image';
+import Div from '../atoms/Div';
+import Checkbox from '../atoms/Checkbox';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <Div style={{ padding: '2rem' }}>
+      <Image src="/assets/icons/mascot.png" style={{ marginTop: '3rem', marginBottom: '3rem' }} />
+      <Div>
+        <Image src="/assets/icons/kakao_login_medium_wide.png" style={{ marginBottom: '1rem' }}></Image>
+        <Checkbox size="large" label="로그인 유지" />
+      </Div>
+    </Div>
+  );
 }
