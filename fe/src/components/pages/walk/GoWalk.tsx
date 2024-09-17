0798a42f0ac44b2f4cba9824b2619ee49950226b
minjeongHEO <hmjoung9770@gmail.com>
2024-09-17 19:41:46 +0900

    feat: play아이콘 및 선택 팝업 추가

--- fe/src/components/pages/walk/GoWalk.tsx
+++ fe/src/components/pages/walk/GoWalk.tsx
@@ -1,7 +1,15 @@
 import { useRef } from 'react';
 import styled from 'styled-components';
 
+import PlayIcon from '@/components/icons/PlayIcon';
+import BuddySelectBar from '@/components/molecules/BuddySelectBar';
 import { useKakaoMap } from '@/hooks/useKakaoMap';
+
+const playIconStyle = {
+  $stroke: 'white',
+  $shadow: '2px 2px 5px rgba(0, 0, 0, 0.5)',
+  $isCursor: true,
+};
 
 export default function GoWalk() {
   const mapRef = useRef<HTMLDivElement | null>(null);
@@ -10,17 +18,27 @@ export default function GoWalk() {
 
   return (
     <StyledWalkWrapper>
+      <StyledPlayIcon customStyle={playIconStyle} />
       <StyledMap ref={mapRef}></StyledMap>
+      <BuddySelectBar />
     </StyledWalkWrapper>
   );
 }
 const StyledWalkWrapper = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
   background-color: aliceblue;
 `;
+
 const StyledMap = styled.div`
   min-width: 100%;
   min-height: 100%;
   object-fit: cover;
+`;
+
+const StyledPlayIcon = styled(PlayIcon)`
+  position: absolute;
+  z-index: 999;
+  bottom: 500px;
 `;
