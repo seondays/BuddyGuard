30e12dae68d845397ee92ff9e4353cfb3e58816b
minjeongHEO <hmjoung9770@gmail.com>
2024-09-17 22:06:53 +0900

    refactor: 이미지 컴포넌트에 텍스트 말줄임 추가

--- fe/src/components/atoms/Image.tsx
+++ fe/src/components/atoms/Image.tsx
@@ -1,39 +1,50 @@
 import styled from 'styled-components';
 
-export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+export interface ImageProps extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, '$$borderRadius'> {
   $borderRadius?: string;
   $boxShadow?: boolean;
   $isClicked?: boolean;
   src: string;
   text?: string;
   textPosition?: 'top' | 'bottom' | 'left' | 'right';
+  $isTextHidden?: boolean;
 }
 
 export default function Image({
-  $borderRadius = '0',
+  className,
+  $borderRadius = 'none',
   $boxShadow = false,
   $isClicked = false,
   src,
   text,
   textPosition,
+  $isTextHidden = true,
   style,
   ...rest
 }: ImageProps) {
   const commonStyle = { fontSize: style?.fontSize, fontWeight: style?.fontWeight, color: style?.color };
   return (
-    <StyledImageContainer>
-      {text && textPosition === 'top' && <StyledText style={commonStyle}>{text}</StyledText>}
+    <StyledImageContainer className={className}>
+      {text && textPosition === 'top' && (
+        <StyledText style={commonStyle} $isTextHidden={$isTextHidden}>
+          {text}
+        </StyledText>
+      )}
 
       <StyledImage {...{ $borderRadius, $boxShadow, $isClicked, src, style, ...rest }} />
 
-      {text && textPosition === 'bottom' && <StyledText style={commonStyle}>{text}</StyledText>}
+      {text && textPosition === 'bottom' && (
+        <StyledText $isTextHidden={$isTextHidden} style={commonStyle}>
+          {text}
+        </StyledText>
+      )}
       {text && textPosition === 'left' && (
-        <StyledText style={commonStyle} position={textPosition}>
+        <StyledText style={commonStyle} position={textPosition} $isTextHidden={$isTextHidden}>
           {text}
         </StyledText>
       )}
       {text && textPosition === 'right' && (
-        <StyledText style={commonStyle} position={textPosition}>
+        <StyledText style={commonStyle} position={textPosition} $isTextHidden={$isTextHidden}>
           {text}
         </StyledText>
       )}
@@ -49,7 +60,7 @@ const StyledImageContainer = styled.div`
   position: relative;
 `;
 
-const StyledText = styled.span<{ position?: 'left' | 'right' }>`
+const StyledText = styled.span<{ position?: 'left' | 'right'; $isTextHidden: boolean }>`
   margin-top: ${({ position }) => (position ? '0' : '0.5rem')};
   margin-left: ${({ position }) => (position === 'left' ? '0.5rem' : '0')};
   margin-right: ${({ position }) => (position === 'right' ? '0.5rem' : '0')};
@@ -60,12 +71,26 @@ const StyledText = styled.span<{ position?: 'left' | 'right' }>`
   transform: ${({ position }) => (position ? 'translateY(-50%)' : 'none')};
   color: ${({ style }) => style?.color || 'red'};
   white-space: nowrap;
+
+  ${({ $isTextHidden }) =>
+    $isTextHidden &&
+    `width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;`};
 `;
 
 const StyledImage = styled.img<ImageProps & { $isClicked: boolean }>`
-  width: ${({ style }) => style?.width || '100%'};
-  height: ${({ style }) => style?.height || 'auto'};
-  border-radius: ${({ $borderRadius }) => $borderRadius};
+  ${({ style }) => {
+    const { width, height, border, marginTop } = style || {};
+    return `
+      width: ${width || '100%'};
+      height: ${height || 'auto'};
+      border: ${border || 'none'};
+      margin-top:${marginTop || 'none'};
+    `;
+  }}
+
+  border-radius: ${({ $borderRadius }) => $borderRadius || '0'};
   background: transparent;
   cursor: pointer;
 
