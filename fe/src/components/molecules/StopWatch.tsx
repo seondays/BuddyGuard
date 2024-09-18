899334c75bad91cd91ab32218425dd66fbf7ac0e
minjeongHEO <hmjoung9770@gmail.com>
2024-09-18 22:53:29 +0900

    refactor: 타이머 로직 헬퍼펑션과 커스텀훅으로 분리 #25

--- fe/src/components/molecules/StopWatch.tsx
+++ fe/src/components/molecules/StopWatch.tsx
@@ -1,5 +1,7 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+
+import { getTimeFormatString } from '@/helper/timerHelpers';
+import useStopWatch from '@/hooks/useStopWatch';
 
 import { StatusOfTime } from './WalkSatusBar';
 
@@ -10,54 +12,7 @@ interface StopWatchProps {
 }
 
 export default function StopWatch({ className, children, status }: StopWatchProps) {
-  const [time, setTime] = useState(0);
-  const timeoutRef = useRef<number | null>(null);
-
-  const startTimer = useCallback(() => {
-    timeoutRef.current = window.setTimeout(() => {
-      setTime((prev) => prev + 1);
-      startTimer();
-    }, 1000);
-  }, []);
-
-  const stopTimer = useCallback(() => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (status === 'start') {
-      startTimer();
-      return;
-    }
-
-    if (status === 'pause') {
-      stopTimer();
-      return;
-    }
-
-    if (status === 'stop') {
-      stopTimer();
-      setTime(0);
-    }
-
-    return () => stopTimer();
-  }, [status, startTimer, stopTimer]);
-
-  const getTimeFormatString = (time: number) => {
-    const hour = Math.floor(time / 3600)
-      .toString()
-      .padStart(2, '0');
-    const min = Math.floor((time % 3600) / 60)
-      .toString()
-      .padStart(2, '0');
-    const sec = Math.floor(time % 60)
-      .toString()
-      .padStart(2, '0');
-
-    return `${hour}:${min}:${sec}`;
-  };
+  const time = useStopWatch(status);
 
   return (
     <StyledStopWatchWrapper className={className}>
---
+++ fe/src/hooks/useStopWatch.ts
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+import { StatusOfTime } from '@/components/molecules/WalkSatusBar';
+
+export default function useStopWatch(status: StatusOfTime) {
+  const [time, setTime] = useState(0);
+  const timeoutRef = useRef<number | null>(null);
+
+  const startTimer = useCallback(() => {
+    timeoutRef.current = window.setTimeout(() => {
+      setTime((prev) => prev + 1);
+      startTimer();
+    }, 1000);
+  }, []);
+
+  const stopTimer = useCallback(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (status === 'start') {
+      startTimer();
+      return;
+    }
+
+    if (status === 'pause') {
+      stopTimer();
+      return;
+    }
+
+    if (status === 'stop') {
+      stopTimer();
+      setTime(0);
+    }
+
+    return () => stopTimer();
+  }, [status, startTimer, stopTimer]);
+
+  return time;
+}
