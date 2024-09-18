eccc34423ef47eec6c5272341743ea692539bc86
shimbaa <shim9597@gmail.com>
2024-09-18 15:48:47 +0900

    refactor: request dto 잘못된 타입 변경 #8

--- be/src/main/java/buddyguard/mybuddyguard/weight/contoller/request/WeightCreateRequest.java
+++ be/src/main/java/buddyguard/mybuddyguard/weight/contoller/request/WeightCreateRequest.java
@@ -3,7 +3,7 @@ package buddyguard.mybuddyguard.weight.contoller.request;
 import java.time.LocalDateTime;
 
 public record WeightCreateRequest(
-        Integer petId,
+        Long petId,
         LocalDateTime recordedAt,
         Double weight,
         String description
