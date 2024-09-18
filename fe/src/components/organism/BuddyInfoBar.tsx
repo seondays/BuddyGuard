6d63ee90592892acb3296cae9e5cccd009a0fc25
jinjaehwan <jjh099@icloud.com>
2024-09-19 00:46:54 +0900

    refactor: BuddyInfoBar 수정

--- fe/src/components/organism/BuddyInfoBar.tsx
+++ fe/src/components/organism/BuddyInfoBar.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Image from '../atoms/Image';
 import Span from '../atoms/Span';
 
 export default function BuddyInfoBar() {
+  const location = useLocation();
+
+  const isHome = location.pathname === '/';
+
   return (
     <div
       style={{
@@ -13,21 +18,24 @@ export default function BuddyInfoBar() {
         borderRadius: '1rem',
       }}
     >
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '1rem' }}>
+      <div id="top-info" style={{ display: 'flex', alignItems: 'center' }}>
         <Image
           src="/assets/icons/defaultBuddy.png"
           style={{ width: '2rem', borderRadius: '50%', marginRight: '1rem' }}
         />
         <Span>버디 이름</Span>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <Span>미해결</Span>
-        <Span>일정</Span>
-        <Span>건강</Span>
-        <Span>체중</Span>
-        <Span>산책</Span>
-        <Span>식사량</Span>
-      </div>
+
+      {isHome && (
+        <div id="bottom-info" style={{ display: 'flex', justifyContent: 'space-around', marginTop: '1rem' }}>
+          <Span>미해결</Span>
+          <Span>일정</Span>
+          <Span>건강</Span>
+          <Span>체중</Span>
+          <Span>산책</Span>
+          <Span>식사량</Span>
+        </div>
+      )}
     </div>
   );
 }
