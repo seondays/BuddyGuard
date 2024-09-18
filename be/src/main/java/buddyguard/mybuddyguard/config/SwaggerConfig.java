77ae64d570487eb75b50c37580d6b6c942cee0e7
seondays <agseon9@gmail.com>
2024-09-18 18:36:09 +0900

    feat : swagger 설정 추가 #15
    swagger에서 api에 접속할 때 토큰을 전달할 수 있도록 Authorize 기능 추가

---
+++ be/src/main/java/buddyguard/mybuddyguard/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package buddyguard.mybuddyguard.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("BuddyGuard")
+                .description("버디가드의 api들을 정리합니다");
+
+        // SecuritySecheme명
+        String jwtSchemeName = "jwtAccess";
+        String jwtRefreshSchemeName = "jwtRefresh";
+
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        SecurityRequirement securityRefreshRequirement = new SecurityRequirement().addList(jwtRefreshSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(In.HEADER)
+                        .name("access"))
+                .addSecuritySchemes(jwtRefreshSchemeName, new SecurityScheme()
+                        .name(jwtRefreshSchemeName)
+                        .type(Type.APIKEY)
+                        .in(In.HEADER)
+                        .name("refresh"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .addSecurityItem(securityRefreshRequirement)
+                .components(components);
+    }
+}
