2d0c56cf78831b1fd3808f3ef07280629f9c8a21
choejieun <wldms5221@icloud.com>
2024-09-20 12:20:33 +0900

    HospitalRecordUpdateRequest: 유효성 검사 수정 NotBlank -> NotNull 변경 Related to: #2

--- be/src/main/java/buddyguard/mybuddyguard/hospital/controller/request/HospitalRecordUpdateRequest.java
+++ be/src/main/java/buddyguard/mybuddyguard/hospital/controller/request/HospitalRecordUpdateRequest.java
@@ -1,11 +1,12 @@
 package buddyguard.mybuddyguard.hospital.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public record HospitalRecordUpdateRequest(
 
-        @NotBlank
+        @NotNull
         LocalDateTime visitDate,
         @NotBlank
         String hospitalName,
