fe08796eb13b552ad9690c0b6a3a8b0587b96362
choejieun <wldms5221@icloud.com>
2024-09-20 09:24:44 +0900

    refactor: toEntity 수정 HospitalId 삭제 Related to: #2

--- be/src/main/java/buddyguard/mybuddyguard/hospital/mapper/HospitalRecordMapper.java
+++ be/src/main/java/buddyguard/mybuddyguard/hospital/mapper/HospitalRecordMapper.java
@@ -1,6 +1,7 @@
 package buddyguard.mybuddyguard.hospital.mapper;
 
 import buddyguard.mybuddyguard.hospital.controller.reponse.HospitalRecordResponse;
+import buddyguard.mybuddyguard.hospital.controller.request.HospitalRecordCreateRequest;
 import buddyguard.mybuddyguard.hospital.entity.HospitalRecord;
 
 public class HospitalRecordMapper {
@@ -16,14 +17,13 @@ public class HospitalRecordMapper {
         );
     }
 
-    public static HospitalRecord toEntity(HospitalRecordResponse hospitalRecordDTO) {
-        return new HospitalRecord(
-                hospitalRecordDTO.id(),
-                hospitalRecordDTO.userId(),
-                hospitalRecordDTO.petId(),
-                hospitalRecordDTO.visitDate(),
-                hospitalRecordDTO.hospitalName(),
-                hospitalRecordDTO.description()
-        );
+    public static HospitalRecord toEntity(Long userId, Long petId, HospitalRecordCreateRequest request) {
+        return HospitalRecord.builder()
+                .userId(userId)
+                .petId(petId)
+                .visitDate(request.visitDate())
+                .hospitalName(request.hospitalName())
+                .description(request.description())
+                .build();
     }
 }
