8b635dd3a406b98b0ff3e65f82543c55cb267b26
minjeongHEO <hmjoung9770@gmail.com>
2024-09-25 16:33:56 +0900

    refactor: 타입파일로  이동 #19

--- fe/src/types/map.ts
+++ fe/src/types/map.ts
@@ -1,1 +1,9 @@
 export type PositionType = [number, number];
+
+export interface PositionPair {
+  previous: PositionType | null;
+  current: PositionType;
+}
+export interface SelctedBuddy {
+  img: string;
+}
