28497e7f49620bbd7d95f58e5522f0123719176d
minjeongHEO <hmjoung9770@gmail.com>
2024-09-29 17:43:50 +0900

    refactor: env 키 네임 변경

--- fe/src/constants/urlConstants.ts
+++ fe/src/constants/urlConstants.ts
@@ -1,3 +1,5 @@
 export const KAKAOMAP_API_SRC = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${
-  import.meta.env.VITE_KAKAOMAP_KEY
+  import.meta.env.VITE_KAKAOMAP_JS_KEY
 }&autoload=false`;
+
+export const STATIC_KAKAOMAP_API_SRC = `https://dapi.kakao.com/v2/maps/staticmap?&appkey=${import.meta.env.VITE_KAKAOMAP_API_KEY}`;
