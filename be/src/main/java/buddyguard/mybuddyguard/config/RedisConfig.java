7e959a31f96d3aa1c6ac3a73938d353c714d8183
seondays <agseon9@gmail.com>
2024-10-01 16:17:20 +0900

    chore: Redis 의존성과 config 설정 #42
    초대링크 정보를 redis에 저장하기 위한 환경 설정입니다. 데이터 소스와의 연결 혼동을 방지하기 위해 레포지토리 스캔 범위를 설정해주었고, host, port 번호를 설정 파일에서 가져와서 사용합니다.

---
+++ be/src/main/java/buddyguard/mybuddyguard/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package buddyguard.mybuddyguard.config;
+
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = {"buddyguard.mybuddyguard.hospital",
+        "buddyguard.mybuddyguard.login", "buddyguard.mybuddyguard.pet",
+        "buddyguard.mybuddyguard.weight", "buddyguard.mybuddyguard.jwt"})
+@EnableRedisRepositories(basePackages = "buddyguard.mybuddyguard.invitation.repository")
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(3))  // 3초동안 Redis 응답 없으면 timeout
+                .shutdownTimeout(Duration.ZERO)         // 종료 시 기다림 없이 바로 종료
+                .build();
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort),
+                clientConfig);
+    }
+}
