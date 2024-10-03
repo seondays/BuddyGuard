80d087f1bf2b1d6a1fb6fe18b825a1a7acab25b2
minjeongHEO <hmjoung9770@gmail.com>
2024-10-03 22:02:14 +0900

    test: WalkBuddySelectBar 스토리북 테스트 #49

--- fe/src/components/templates/ResponsiveLayout.tsx
+++ fe/src/components/templates/ResponsiveLayout.tsx
@@ -72,7 +72,7 @@ const StyledDescriptionArea = styled.article`
   }
 `;
 
-const StyledMobileFrame = styled.div`
+export const StyledMobileFrame = styled.div`
   position: relative;
   width: 90vw;
   height: 90vh;
