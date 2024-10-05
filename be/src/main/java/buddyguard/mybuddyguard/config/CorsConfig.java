6808744f3c96835f883f68b3a1dfb0cf850fcfad
choejieun <wldms5221@icloud.com>
2024-10-06 00:23:21 +0900

    refactor: CorsConfig 수정

--- be/src/main/java/buddyguard/mybuddyguard/config/CorsConfig.java
+++ be/src/main/java/buddyguard/mybuddyguard/config/CorsConfig.java
@@ -10,6 +10,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins(
+                        "http://192.168.45.65:5173",  // 로컬 IP 주소
+                        "http://buddyguard.site:5173" // 도메인 주소
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 설정
+                .allowedHeaders("*") // 허용할 헤더 설정
+                .allowCredentials(true); // 인증 정보 허용
     }
 }
