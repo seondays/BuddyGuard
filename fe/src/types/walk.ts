5e369f19d43cd83c72afacc53e9520111f1cff02
minjeongHEO <hmjoung9770@gmail.com>
2024-10-10 22:18:00 +0900

    feat: 산책 타입 파일 생성  #70

---
+++ fe/src/types/walk.ts
@@ -0,0 +1,19 @@
+export interface Buddy {
+  id: number;
+  image: string;
+}
+
+export interface WalkData {
+  date: string;
+  distance: number;
+  totalTime: string;
+  buddys: Buddy[];
+  pathImage: string;
+}
+
+export interface WalkMockData {
+  count: number;
+  averageDistance: number;
+  averageTime: string;
+  data: WalkData[];
+}
