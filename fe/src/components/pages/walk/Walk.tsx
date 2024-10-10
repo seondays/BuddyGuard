cba0c1542609febbf1deb2c8a333e490d2a0e433
minjeongHEO <hmjoung9770@gmail.com>
2024-10-10 13:13:24 +0900

    feat: 기간필터 컴포넌트 적용 #69

--- fe/src/components/pages/walk/Walk.tsx
+++ fe/src/components/pages/walk/Walk.tsx
@@ -1,4 +1,5 @@
 import PageTitleBar from '@/components/molecules/PageTitleBar';
+import PeriodFilter from '@/components/molecules/PeriodFilter';
 import BuddyInfoBar from '@/components/organisms/BuddyInfoBar';
 
 export default function Walk() {
@@ -6,6 +7,7 @@ export default function Walk() {
     <div style={{ padding: '1rem', height: '100vh' }}>
       <PageTitleBar title="산책 관리" />
       <BuddyInfoBar />
+      <PeriodFilter />
       <div style={{ height: '70vh', overflowY: 'scroll', scrollbarWidth: 'none' }}></div>
     </div>
   );
