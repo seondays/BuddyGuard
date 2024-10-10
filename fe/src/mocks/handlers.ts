5c738e21e66b373322ea71ef6df2e6cc399041d9
minjeongHEO <hmjoung9770@gmail.com>
2024-10-10 22:19:38 +0900

    feat: 산책 활동 목데이터 헨들러 추가 #70

--- fe/src/mocks/handlers.ts
+++ fe/src/mocks/handlers.ts
@@ -1,5 +1,78 @@
 import { http, HttpResponse } from 'msw';
 
-const mockData = {};
+import { WalkMockData } from '@/types/walk';
 
-export const handlers = [http.get('/api/', () => HttpResponse.json(mockData))];
+const walkMockDataWeek: WalkMockData = {
+  count: 2,
+  averageDistance: 2.8,
+  averageTime: '10:10:02',
+  data: [
+    {
+      date: '2024-08-30',
+      distance: 2.8,
+      totalTime: '00:30:00',
+      buddys: [
+        { id: 1, image: 'https://via.placeholder.com/100' },
+        { id: 2, image: 'https://via.placeholder.com/100' },
+      ],
+      pathImage: 'https://via.placeholder.com/200',
+    },
+    {
+      date: '2024-08-29',
+      distance: 2.8,
+      totalTime: '00:30:00',
+      buddys: [{ id: 1, image: 'https://via.placeholder.com/100' }],
+      pathImage: 'https://via.placeholder.com/200',
+    },
+  ],
+};
+const walkMockDataMonth: WalkMockData = {
+  count: 4,
+  averageDistance: 2.8,
+  averageTime: '10:10:02',
+  data: [
+    {
+      date: '2024-08-01',
+      distance: 5,
+      totalTime: '00:30:00',
+      buddys: [
+        { id: 1, image: 'https://via.placeholder.com/100' },
+        { id: 2, image: 'https://via.placeholder.com/100' },
+      ],
+      pathImage: 'https://via.placeholder.com/200',
+    },
+    {
+      date: '2024-08-02',
+      distance: 2.8,
+      totalTime: '00:30:00',
+      buddys: [
+        { id: 1, image: 'https://via.placeholder.com/100' },
+        { id: 2, image: 'https://via.placeholder.com/100' },
+      ],
+      pathImage: 'https://via.placeholder.com/200',
+    },
+    {
+      date: '2024-08-18',
+      distance: 6,
+      totalTime: '01:30:00',
+      buddys: [
+        { id: 1, image: 'https://via.placeholder.com/100' },
+        { id: 2, image: 'https://via.placeholder.com/100' },
+      ],
+      pathImage: 'https://via.placeholder.com/200',
+    },
+    {
+      date: '2024-08-29',
+      distance: 0.1,
+      totalTime: '00:01:00',
+      buddys: [{ id: 1, image: 'https://via.placeholder.com/100' }],
+      pathImage: 'https://via.placeholder.com/200',
+    },
+  ],
+};
+
+export const handlers = [
+  http.get('/api/walks/week', () => HttpResponse.json(walkMockDataWeek)),
+  http.get('/api/walks/month/10', () => HttpResponse.json(walkMockDataMonth)),
+  http.get('/api/walks/month/10', () => HttpResponse.json(walkMockDataMonth)),
+];
