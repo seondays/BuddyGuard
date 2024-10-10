5f6c0c28bccadee2c445cf0620986825ffc914f1
minjeongHEO <hmjoung9770@gmail.com>
2024-10-10 13:12:21 +0900

    feat: 공통 레이아웃 스타일 파일 생성

---
+++ fe/src/styles/layoutStyles.ts
@@ -0,0 +1,19 @@
+import { css } from 'styled-components';
+
+export const flexRow = css`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const flexColumn = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const flexCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
