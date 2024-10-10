34bea3ce270f0f217a0886a7dca5f3b251a632a5
minjeongHEO <hmjoung9770@gmail.com>
2024-10-10 12:56:31 +0900

    feat: arrow_back svg추가 및 페이지 타이틀바 수정 #69

--- fe/src/components/molecules/PageTitleBar.tsx
+++ fe/src/components/molecules/PageTitleBar.tsx
@@ -1,11 +1,34 @@
-import Image from '../atoms/Image';
-import Span from '../atoms/Span';
+import { useNavigate } from 'react-router-dom';
+import styled, { useTheme } from 'styled-components';
+
+import Span from '@/components/atoms/Span';
+import arrow_back from '@/svg/arrow_back.svg';
 
 export default function PageTitleBar({ title }: { title: string }) {
+  const titleColor = useTheme().currentTheme.textPrimary;
+  const navigate = useNavigate();
+
+  const handleClick = () => navigate('/menu');
+
   return (
-    <div style={{ display: 'flex', alignItems: 'center', position: 'relative', padding: '1rem' }}>
-      <Image src="/assets/icons/arrow_back.png" style={{ width: '1rem', position: 'absolute', left: 0 }} />
-      <Span style={{ margin: '0 auto', fontSize: '1.2rem', fontWeight: 'bold' }}>{title}</Span>
+    <div style={{ display: 'flex', alignItems: 'flex-start;', position: 'relative', padding: '1rem' }}>
+      <div onClick={handleClick}>
+        <StyledArrowBack />
+      </div>
+      <Span $color={titleColor} style={{ margin: '0 auto', fontSize: '1.5rem', fontWeight: 'bold' }}>
+        {title}
+      </Span>
     </div>
   );
 }
+
+const StyledArrowBack = styled(arrow_back)`
+  position: absolute;
+  left: 0;
+  width: 2rem;
+  cursor: pointer;
+
+  & path {
+    stroke: ${({ theme }) => theme.currentTheme.textSecondary};
+  }
+`;
