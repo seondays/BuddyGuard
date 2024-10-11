80f39345cc4c17fe3491790c9b413f9eb41394f0
minjeongHEO <hmjoung9770@gmail.com>
2024-10-11 15:41:32 +0900

    test: 산책 리스트박스 스토리북 테스트 #69

---
+++ fe/src/components/molecules/walk/ListBox.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import ListBox, { ListBoxProps } from '@/components/molecules/walk/ListBox';
+
+export default {
+  title: 'Molecules/ListBox',
+  component: ListBox,
+  argTypes: {},
+} as Meta;
+
+// 템플릿 생성
+const Template: StoryFn<ListBoxProps> = (args) => {
+  return <ListBox />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  status: 'start',
+  children: '시작',
+};
