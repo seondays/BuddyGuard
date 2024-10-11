ba9d25a96de9ce6fc7d78813bbaf4e26b124eaec
minjeongHEO <hmjoung9770@gmail.com>
2024-10-11 22:04:26 +0900

    feat: 홈화면에서 accessToken 요청 구현 #54

--- fe/src/components/pages/Home.tsx
+++ fe/src/components/pages/Home.tsx
@@ -1,14 +1,22 @@
+import { useEffect } from 'react';
+
 import BuddyInfoBar from '@/components/organisms/BuddyInfoBar';
 import DashBoardList from '@/components/organisms/DashBoardList';
+import { useAuthMutation } from '@/hooks/useAuthQuery';
 
 export default function Home() {
+  const { getAccessToken, isPending, status } = useAuthMutation();
+
+  useEffect(() => {
+    getAccessToken();
+  }, [getAccessToken]);
+  if (isPending) return <div>로그인 중입니다...</div>;
+
   return (
     <div style={{ padding: '1rem' }}>
       <BuddyInfoBar />
       <div style={{ height: '75vh', overflowY: 'scroll', scrollbarWidth: 'none' }}>
         <DashBoardList />
-
-        <a href="api/buddyguard.site/login">로그인</a>
       </div>
     </div>
   );
---
+++ fe/src/hooks/useAuthQuery.ts
@@ -0,0 +1,27 @@
+import { useMutation } from '@tanstack/react-query';
+
+import apiClient from '@/apis/axiosInstance';
+
+const fetchAccessToken = async () => {
+  const response = await apiClient.post('/reissue');
+  return response.data.accessToken;
+};
+
+export const useAuthMutation = () => {
+  const {
+    mutate: getAccessToken,
+    isPending,
+    status,
+  } = useMutation({
+    mutationFn: fetchAccessToken,
+    onSuccess: (newAccessToken) => {
+      localStorage.setItem('accessToken', newAccessToken);
+    },
+    onError: () => {
+      window.location.href = '/join';
+      localStorage.removeItem('accessToken');
+    },
+  });
+
+  return { getAccessToken, isPending, status };
+};
