8a40946129209f12746198597be335c7a27f53e8
minjeongHEO <hmjoung9770@gmail.com>
2024-10-11 21:33:52 +0900

    refactor: 개발모드 일 때ReactQueryDevtools #70

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 
-import useFirebaseMessaging from './hooks/useFirebaseMessaging';
+// import useFirebaseMessaging from './hooks/useFirebaseMessaging';
 import { router } from './routes/router';
 import { GlobalStyles } from './styles/globalStyles';
 import { ResetStyles } from './styles/resetStyles';
@@ -17,7 +17,7 @@ function App() {
       <GlobalStyles />
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-        {import.meta.env.VITE_APP_ENV === 'development' && <ReactQueryDevtools initialIsOpen={true} />}
+        {import.meta.env.MODE === 'development' && <ReactQueryDevtools initialIsOpen={true} position="left" />}
       </QueryClientProvider>
     </>
   );
