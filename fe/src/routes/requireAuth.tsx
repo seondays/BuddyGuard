ae163b859295652e900903f1f5df299436688164
minjeongHEO <hmjoung9770@gmail.com>
2024-10-11 22:01:57 +0900

    feat: 모든 페이지에서 로그인 여부를 체크하는 requireAuth 컴포넌트생성 #54

---
+++ fe/src/routes/requireAuth.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+export default function RequireAuth({ children }: { children: React.ReactNode }) {
+  const accessToken = localStorage.getItem('accessToken');
+
+  if (!accessToken) return <Navigate to="/join" />;
+
+  return <>{children}</>;
+}
