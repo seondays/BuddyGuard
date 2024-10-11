11d3dfb9d4f733fa065c92c8ed6669a6161e7f31
minjeongHEO <hmjoung9770@gmail.com>
2024-10-11 15:40:59 +0900

    chore: react-query-devtools 추가 및 provider, devtools적용 #70

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,15 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './routes/router';
 import { GlobalStyles } from './styles/globalStyles';
 import { ResetStyles } from './styles/resetStyles';
 
+const queryClient = new QueryClient();
 function App() {
   return (
     <>
       <ResetStyles />
       <GlobalStyles />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        {import.meta.env.VITE_APP_ENV === 'development' && <ReactQueryDevtools initialIsOpen={true} />}
+      </QueryClientProvider>
     </>
   );
 }
