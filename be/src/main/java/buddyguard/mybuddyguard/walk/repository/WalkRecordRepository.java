b86fea210332aaf64e4043ca840cde362022c89d
choejieun <wldms5221@icloud.com>
2024-10-14 13:06:09 +0900

    refactor: WalkRecordRepository 수정 @repository 추가
    related to : #38

--- be/src/main/java/buddyguard/mybuddyguard/walk/repository/WalkRecordRepository.java
+++ be/src/main/java/buddyguard/mybuddyguard/walk/repository/WalkRecordRepository.java
@@ -3,9 +3,10 @@ package buddyguard.mybuddyguard.walk.repository;
 import buddyguard.mybuddyguard.walk.entity.WalkRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WalkRecordRepository extends JpaRepository<WalkRecord, Long> {
 
     Optional<WalkRecord> findById(Long id);
