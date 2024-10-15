a3ca4fc1524786511011f40fc7ea337ca1063344
shimbaa <shim9597@gmail.com>
2024-10-15 16:46:03 +0900

    docs: swagger 설명 추가

--- be/src/main/java/buddyguard/mybuddyguard/weight/contoller/WeightController.java
+++ be/src/main/java/buddyguard/mybuddyguard/weight/contoller/WeightController.java
@@ -4,6 +4,7 @@ import buddyguard.mybuddyguard.weight.contoller.request.WeightCreateRequest;
 import buddyguard.mybuddyguard.weight.contoller.response.WeightResponse;
 import buddyguard.mybuddyguard.weight.contoller.request.WeightUpdateRequest;
 import buddyguard.mybuddyguard.weight.service.WeightService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,7 @@ public class WeightController {
 
     private final WeightService weightService;
 
+    @Operation(summary = "해당 id pet의 모든 체중 기록 조회")
     @GetMapping("/{petId}")
     public ResponseEntity<List<WeightResponse>> getAllWeightRecords(
             @PathVariable("petId") Long petId) {
@@ -33,6 +35,7 @@ public class WeightController {
         return ResponseEntity.ok(responses);
     }
 
+    @Operation(summary = "체중 단일 조회", description = "pet id는 체중이 해당 pet의 기록이 맞는지 검증 위해 쓰임.")
     @GetMapping("/{petId}/detail/{id}")
     public ResponseEntity<WeightResponse> getDetailWeightRecord(
             @PathVariable("petId") Long petId,
@@ -42,6 +45,7 @@ public class WeightController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "체중 기록 생성")
     @PostMapping
     public ResponseEntity<Void> createNewWeightRecord(@RequestBody @Valid WeightCreateRequest request) {
 
@@ -49,6 +53,7 @@ public class WeightController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "체중 기록 수정", description = "pet id는 체중이 해당 pet의 기록이 맞는지 검증 위해 쓰임.")
     @PatchMapping("/{petId}/detail/{id}")
     public ResponseEntity<Void> updateWeightRecord(
             @PathVariable("petId") Long petId,
@@ -60,6 +65,7 @@ public class WeightController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "체중 기록 삭제", description = "pet id는 체중이 해당 pet의 기록이 맞는지 검증 위해 쓰임.")
     @DeleteMapping("/{petId}/detail/{id}")
     public ResponseEntity<Void> deleteWeightRecord(
             @PathVariable("petId") Long petId,
