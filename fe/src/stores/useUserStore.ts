9a1f43e5d5f4d3feafe1878a63695c0655d8ce86
minjeongHEO <hmjoung9770@gmail.com>
2024-10-16 09:23:20 +0900

    reactor: store에 duddysData추가 #81

--- fe/src/stores/useUserStore.ts
+++ fe/src/stores/useUserStore.ts
@@ -1,7 +1,10 @@
 import { create } from 'zustand';
 
+type buddyData = { id: number; name: string; age: number; image: string };
+
 type State = {
   buddyID: number;
+  buddysData: buddyData[];
   hasMultiUser: boolean;
 };
 type Action = {
@@ -10,6 +13,7 @@ type Action = {
 
 export const useUserStore = create<State & Action>((set) => ({
   buddyID: 0,
+  buddysData: [{ id: 0, name: '', age: 0, image: '' }],
   hasMultiUser: false,
   changeBuddy: (newBuddy: number) => set({ buddyID: newBuddy }),
 }));
