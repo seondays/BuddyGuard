db7142f03e69b164c9f899166a88b35b61fe18c2
minjeongHEO <hmjoung9770@gmail.com>
2024-10-17 02:24:56 +0900

    feat: nav에 svg적용

--- fe/src/components/organisms/Nav.tsx
+++ fe/src/components/organisms/Nav.tsx
@@ -1,17 +1,21 @@
 import { Link } from 'react-router-dom';
-import { useTheme } from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 
-import Image from '@/components/atoms/Image';
 import Span from '@/components/atoms/Span';
+import Notification from '@/svg/nav_icon_bell.svg';
+import Home from '@/svg/nav_icon_home.svg';
+import GoWalk from '@/svg/nav_icon_map.svg';
+import menu from '@/svg/nav_icon_menu.svg';
+import MyPage from '@/svg/nav_icon_people.svg';
 
 export const NAV_HEIGHT = '4rem';
 
 const navItems = [
-  { to: '/', src: '/assets/icons/home.png', label: '홈' },
-  { to: '/menu/walk/go', src: '/assets/icons/walk.png', label: '산책' },
-  { to: '/menu', src: '/assets/icons/menu.png', label: '메뉴' },
-  { to: '/notification', src: '/assets/icons/notification.png', label: '알림' },
-  { to: '/MyPage', src: '/assets/icons/myPage.png', label: '내 정보' },
+  { to: '/', Icon: Home, label: '홈' },
+  { to: '/menu/walk/go', Icon: GoWalk, label: '산책' },
+  { to: '/menu', Icon: menu, label: '메뉴' },
+  { to: '/notification', Icon: Notification, label: '알림' },
+  { to: '/MyPage', Icon: MyPage, label: '내 정보' },
 ];
 
 export default function Nav() {
@@ -30,10 +34,10 @@ export default function Nav() {
         color: navTextColor,
       }}
     >
-      {navItems.map((item) => (
+      {navItems.map(({ to, Icon, label }) => (
         <Link
-          key={item.to}
-          to={item.to}
+          key={to}
+          to={to}
           style={{
             display: 'flex',
             flexDirection: 'column',
@@ -42,10 +46,19 @@ export default function Nav() {
             flexBasis: '20%',
           }}
         >
-          <Image src={item.src} style={{ fontSize: '0.8rem', width: '1.5rem' }} />
-          <Span $color={navTextColor}>{item.label}</Span>
+          <StyledIcon as={Icon} />
+          <Span $color={navTextColor}>{label}</Span>
         </Link>
       ))}
     </div>
   );
 }
+
+const StyledIcon = styled.svg`
+  width: 1.7rem;
+  height: 1.7rem;
+
+  & path {
+    fill: ${({ theme }) => theme.currentTheme.textPrimary};
+  }
+`;
