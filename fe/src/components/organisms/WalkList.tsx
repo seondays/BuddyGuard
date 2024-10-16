e68120a48de8bf966c38a1613a1f45062fe25d36
minjeongHEO <hmjoung9770@gmail.com>
2024-10-16 21:20:25 +0900

    refactor: 반응형 레이아웃 및 스크롤 적용 #70

--- fe/src/components/organisms/WalkList.tsx
+++ fe/src/components/organisms/WalkList.tsx
@@ -1,30 +1,46 @@
 import styled from 'styled-components';
 
-import ListBox from '../molecules/walk/ListBox';
+import ListBox from '@/components/molecules/walk/ListBox';
+import { flexColumn } from '@/styles/layoutStyles';
 
 export default function WalkList() {
   return (
     <StyledWalkListContainer>
       <StyledTitle>산책 리스트</StyledTitle>
 
-      <ListBox />
-      <ListBox />
-      <ListBox />
-      <ListBox />
-      <ListBox />
+      <StyledListWrapper>
+        <ListBox />
+        <ListBox />
+        <ListBox />
+        <ListBox />
+        <ListBox />
+        <ListBox />
+        <ListBox />
+      </StyledListWrapper>
     </StyledWalkListContainer>
   );
 }
+
+const StyledListWrapper = styled.div`
+  overflow-y: auto;
+  padding: 0 0.3rem;
+  /* 남은 공간을 모두 차지 */
+  flex-grow: 1;
+`;
 
 const StyledTitle = styled.div`
   font-size: 1rem;
   font-weight: bold;
   color: ${({ theme }) => theme.currentTheme.textPrimary};
-  margin-bottom: 1rem;
+  margin-bottom: 0.5rem;
+  height: 10%;
+  /* 크기가 줄어들지 않도록 설정 */
+  flex-shrink: 0;
 `;
 const StyledWalkListContainer = styled.div`
   background-color: ${({ theme }) => theme.currentTheme.grayLighter};
   width: 100%;
   height: 100%;
-  padding: 1rem;
+  padding: 1rem 1rem 0.5rem 1rem;
+  ${flexColumn}
 `;
