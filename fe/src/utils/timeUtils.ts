b92334ef1c4a32d931aabce629ea41f85f7f7313
minjeongHEO <hmjoung9770@gmail.com>
2024-10-16 22:06:29 +0900

    refactor: date를 string으로 받을 때 포맷팅하도록 수정 #70

--- fe/src/utils/timeUtils.ts
+++ fe/src/utils/timeUtils.ts
@@ -1,4 +1,9 @@
-export const getCurrentDate = (isDay = true, isTime = false) => {
+interface getCurrentDateProps {
+  isDay: boolean;
+  isTime: boolean;
+  dateString?: string;
+}
+export const getCurrentDate = ({ isDay = true, isTime = false, dateString }: getCurrentDateProps) => {
   const timeOptions: Intl.DateTimeFormatOptions = { timeZone: 'Asia/Seoul' };
 
   if (isDay) {
@@ -13,6 +18,10 @@ export const getCurrentDate = (isDay = true, isTime = false) => {
     timeOptions.minute = 'numeric';
     timeOptions.hourCycle = 'h23';
   }
-  const today = new Intl.DateTimeFormat('ko-KR', timeOptions).format(new Date());
-  return today;
+
+  // dateString이 없으면 현재 날짜를 사용
+  const date = dateString ? new Date(dateString) : new Date();
+  const formattedDate = new Intl.DateTimeFormat('ko-KR', timeOptions).format(date);
+
+  return formattedDate;
 };
