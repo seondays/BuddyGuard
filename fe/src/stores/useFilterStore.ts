b2dcd207d56a76218bcfcafbc896967d1cd85cb6
minjeongHEO <hmjoung9770@gmail.com>
2024-10-17 10:57:33 +0900

    refactor: 스토어에 필터 타입 변경하는 함수 추가 #70

--- fe/src/stores/useFilterStore.ts
+++ fe/src/stores/useFilterStore.ts
@@ -6,6 +6,7 @@ import { FilterType } from '@/types/walk';
 interface FilterState {
   type: FilterType;
   month: number;
+  setType: (type: FilterType) => void;
   setWeekly: () => void;
   setMonthly: () => void;
   setAll: (month: number) => void;
@@ -14,6 +15,7 @@ interface FilterState {
 export const useFilterStore = create<FilterState>((set) => ({
   type: 'weekly',
   month: dayjs().month() + 1,
+  setType: (type: FilterType) => set({ type }),
   setWeekly: () => set({ type: 'weekly' }),
   setMonthly: () => set({ type: 'monthly' }),
   setAll: (month: number) => set({ type: 'all', month }),
