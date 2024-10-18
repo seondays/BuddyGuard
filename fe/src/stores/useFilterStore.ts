62abeec98e1bca1362845e465e655df28a3219a4
minjeongHEO <hmjoung9770@gmail.com>
2024-10-18 09:22:32 +0900

    refactor: 필터 스토어에 month, year 추가 #70

--- fe/src/stores/useFilterStore.ts
+++ fe/src/stores/useFilterStore.ts
@@ -6,6 +6,9 @@ import { FilterType } from '@/types/walk';
 interface FilterState {
   type: FilterType;
   month: number;
+  year: number;
+  setMonth: (month: number) => void;
+  setYear: (year: number) => void;
   setType: (type: FilterType) => void;
   setWeekly: () => void;
   setMonthly: () => void;
@@ -15,6 +18,9 @@ interface FilterState {
 export const useFilterStore = create<FilterState>((set) => ({
   type: 'weekly',
   month: dayjs().month() + 1,
+  year: dayjs().year(),
+  setMonth: (month: number) => set({ month }),
+  setYear: (year: number) => set({ year }),
   setType: (type: FilterType) => set({ type }),
   setWeekly: () => set({ type: 'weekly' }),
   setMonthly: () => set({ type: 'monthly' }),
