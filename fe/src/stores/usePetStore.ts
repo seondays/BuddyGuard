4e1a71d438429f11a585992ad31a6a7e94dc8cbc
jinjaehwan <jjh099@icloud.com>
2024-10-19 17:21:00 +0900

    feat: 펫정보 로컬스토리지 연동

--- fe/src/stores/usePetStore.ts
+++ fe/src/stores/usePetStore.ts
@@ -1,13 +1,38 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-import { PetInfo } from '@/types/pet';
+type PetInfo = {
+  userId: number;
+  petId: number;
+  petName: string;
+  profileImage: string;
+};
 
 type PetState = {
   petsInfo: PetInfo[];
   setPetsInfo: (pets: PetInfo[]) => void;
 };
 
-export const usePetStore = create<PetState>((set) => ({
-  petsInfo: [],
-  setPetsInfo: (pets) => set({ petsInfo: pets }),
-}));
+export const usePetStore = create<PetState>()(
+  persist(
+    (set) => ({
+      petsInfo: [],
+      setPetsInfo: (pets) => set({ petsInfo: pets }),
+    }),
+    {
+      name: 'petsStorage',
+      storage: {
+        getItem: (name) => {
+          const item = localStorage.getItem(name);
+          return item ? JSON.parse(item) : null;
+        },
+        setItem: (name, value) => {
+          localStorage.setItem(name, JSON.stringify(value));
+        },
+        removeItem: (name) => {
+          localStorage.removeItem(name);
+        },
+      },
+    }
+  )
+);
