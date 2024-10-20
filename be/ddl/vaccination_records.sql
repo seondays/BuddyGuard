bfb7eadba272ecd80ae95bdd4140c5a753679f80
shimbaa <shim9597@gmail.com>
2024-10-20 22:06:00 +0900

    refactor: ddl의 userId 삭제 #120

--- be/ddl/vaccination_records.sql
+++ be/ddl/vaccination_records.sql
@@ -1,7 +1,6 @@
 CREATE TABLE VACCINATION_RECORDS
 (
     id               INTEGER PRIMARY KEY AUTO_INCREMENT, -- 기본 키, 자동 증가
-    user_id          INTEGER      NOT NULL,              -- 사용자 ID (외래 키)
     pet_id           INTEGER      NOT NULL,              -- 반려동물 ID (외래 키)
     vaccination_date DATE         NOT NULL,              -- 예방접종 날짜
     vaccination_name VARCHAR(255) NOT NULL,              -- 백신 이름
