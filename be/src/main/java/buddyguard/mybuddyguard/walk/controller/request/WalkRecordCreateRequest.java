5ed9ec8df1b8c1a5963204ee5f0e6dab2c7cc724
choejieun <wldms5221@icloud.com>
2024-10-20 15:07:39 +0900

    refactor: WalkRecordCreateRequest 수정 toWalkRecord 메서드 추가
    related to : #38

--- be/src/main/java/buddyguard/mybuddyguard/walk/controller/request/WalkRecordCreateRequest.java
+++ be/src/main/java/buddyguard/mybuddyguard/walk/controller/request/WalkRecordCreateRequest.java
@@ -1,10 +1,13 @@
 package buddyguard.mybuddyguard.walk.controller.request;
 
+import buddyguard.mybuddyguard.walk.entity.WalkRecord;
 import buddyguard.mybuddyguard.walk.entity.WalkRecordCenterPosition;
 import buddyguard.mybuddyguard.walk.entity.WalkRecordPath;
+import buddyguard.mybuddyguard.walkimage.entity.WalkS3Image;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -36,6 +39,27 @@ public record WalkRecordCreateRequest(
         Double distance     // 총 거리 (km 단위)
 
 ) {
+    public WalkRecord toWalkRecord(Long recordId, WalkS3Image walkS3Image) {
+        return WalkRecord.builder()
+                .id(recordId)
+                .startDate(this.startDate())  // 산책 시작 날짜
+                .endDate(this.endDate())      // 산책 종료 날짜
+                .startTime(this.startTime())  // 산책 시작 시간
+                .endTime(this.endTime())      // 산책 종료 시간
+                .totalTime(this.totalTime())  // 총 산책 시간
+                .note(this.note())            // 산책에 대한 메모
+                .centerPosition(this.centerPosition().toWalkRecordCenterPosition())  // 중심 위치
+                .mapLevel(this.mapLevel())    // 지도 레벨
+                .distance(this.distance())    // 총 거리
+                .path(this.path().stream()    // 산책 경로
+                        .map(WalkRecordPathRequest::toWalkRecordPath)
+                        .toList()
+                )
+                .pathImage(walkS3Image)       // S3 이미지 설정
+                .petWalkRecords(new ArrayList<>()) // 빈 리스트로 초기화
+                .build();
+    }
+
     @Getter
     @AllArgsConstructor
     @NoArgsConstructor(force = true)
@@ -59,6 +83,8 @@ public record WalkRecordCreateRequest(
                     .longitude(this.longitude)
                     .build();
         }
+
+
 
     }
 }
