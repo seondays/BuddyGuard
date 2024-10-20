b6a3c264b3ffcb167e86b01e4ab9bf25f1c24ff8
seondays <agseon9@gmail.com>
2024-10-20 17:07:44 +0900

    feat: 펫 등록 시 프로필 사진 없이도 등록할 수 있도록 구조 변경

--- be/src/main/java/buddyguard/mybuddyguard/pet/mapper/PetMapper.java
+++ be/src/main/java/buddyguard/mybuddyguard/pet/mapper/PetMapper.java
@@ -2,13 +2,14 @@ package buddyguard.mybuddyguard.pet.mapper;
 
 import buddyguard.mybuddyguard.pet.contoller.request.PetRegisterRequest;
 import buddyguard.mybuddyguard.pet.entity.Pet;
+import java.util.Optional;
 
 public class PetMapper {
 
     public static Pet toEntity(PetRegisterRequest petRegisterRequest) {
         return Pet.builder()
                 .name(petRegisterRequest.name())
-                .profileImage(petRegisterRequest.profileImage())
+                .profileImage(Optional.ofNullable(petRegisterRequest.profileImage()).orElse("None"))
                 .type(petRegisterRequest.type())
                 .birth(petRegisterRequest.birth())
                 .build();
