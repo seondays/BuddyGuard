7a03be22cba65a3f5a9093b891b717d9c59ce766
jinjaehwan <jjh099@icloud.com>
2024-10-20 16:54:58 +0900

    feat: 메뉴 스타일 수정

--- fe/src/components/pages/Hospital.tsx
+++ fe/src/components/pages/Hospital.tsx
@@ -1,14 +1,29 @@
 import PageTitleBar from '@/components/molecules/PageTitleBar';
 import BuddyInfoBar from '@/components/organisms/BuddyInfoBar';
 import HospitalList from '@/components/organisms/HospitalList';
+import { theme } from '@/styles/theme';
+
+import Button from '../atoms/Button';
 
 export default function Hospital() {
   return (
     <div style={{ padding: '1rem', height: '100vh' }}>
       <PageTitleBar title="건강 관리" />
       <BuddyInfoBar />
-      <div style={{ height: '70vh', overflowY: 'scroll', scrollbarWidth: 'none' }}>
+      <div style={{ padding: '1rem', height: '60vh', overflowY: 'scroll', scrollbarWidth: 'none' }}>
         <HospitalList />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button
+          style={{
+            fontWeight: 'bold',
+            width: '50%',
+            border: `0.2rem solid ${theme.colorValues.special.textForce02}`,
+            borderRadius: '10rem',
+          }}
+        >
+          병원 기록 등록
+        </Button>
       </div>
     </div>
   );
