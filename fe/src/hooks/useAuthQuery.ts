4500a7757f87e9ff38c670e035a0bbce0abac92b
minjeongHEO <hmjoung9770@gmail.com>
2024-10-20 17:21:46 +0900

    style: 임시로 넣어둔 delay삭제

--- fe/src/hooks/useAuthQuery.ts
+++ fe/src/hooks/useAuthQuery.ts
@@ -1,7 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { fetchAccessToken } from '@/apis/authAPI';
-import { delay } from '@/utils/utils';
 
 export const useAuthMutation = () => {
   const { mutate: getAccessToken, status } = useMutation({
@@ -10,7 +9,6 @@ export const useAuthMutation = () => {
       localStorage.setItem('accessToken', newAccessToken);
     },
     onError: async () => {
-      await delay(5000);
       localStorage.removeItem('accessToken');
       window.location.href = '/join';
     },
