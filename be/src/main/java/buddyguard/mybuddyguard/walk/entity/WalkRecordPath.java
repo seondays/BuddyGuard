e26c064a6bc0891aabaf4b977374f83a1cefd907
choejieun <wldms5221@icloud.com>
2024-10-20 15:08:56 +0900

    refactor: WalkRecordPath 수정 WalkRecord 컬럼 추가
    related to : #38

--- be/src/main/java/buddyguard/mybuddyguard/walk/entity/WalkRecordPath.java
+++ be/src/main/java/buddyguard/mybuddyguard/walk/entity/WalkRecordPath.java
@@ -1,10 +1,15 @@
 package buddyguard.mybuddyguard.walk.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -31,4 +36,9 @@ public class WalkRecordPath {
     // 경로의 경도
     @Column(name = "longitude", nullable = false)
     private Double longitude;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "walk_record_id", nullable = false , foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private WalkRecord walkRecord;
+
 }
