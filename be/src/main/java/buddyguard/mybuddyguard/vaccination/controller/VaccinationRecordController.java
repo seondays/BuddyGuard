4c69f800ffa9c203e6e0809cc4eb112bfd67c346
shimbaa <shim9597@gmail.com>
2024-10-21 17:18:36 +0900

    feat: 예방접종 전체, 상세 조회 기능 구현 #120

--- be/src/main/java/buddyguard/mybuddyguard/vaccination/controller/VaccinationRecordController.java
+++ be/src/main/java/buddyguard/mybuddyguard/vaccination/controller/VaccinationRecordController.java
@@ -1,11 +1,16 @@
 package buddyguard.mybuddyguard.vaccination.controller;
 
-import buddyguard.mybuddyguard.vaccination.controller.request.VaccinationCreateRequest;
+import buddyguard.mybuddyguard.vaccination.controller.request.VaccinationRecordCreateRequest;
+import buddyguard.mybuddyguard.vaccination.controller.response.VaccinationRecordResponse;
 import buddyguard.mybuddyguard.vaccination.service.VaccinationRecordService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +23,33 @@ public class VaccinationRecordController {
 
     private final VaccinationRecordService vaccinationRecordService;
 
+    @Operation(summary = "예방접종 기록 생성", description = "petId 별 예방접종 기록 생성")
     @PostMapping
-    public ResponseEntity<Void> createVaccinationRecord(@Valid @RequestBody VaccinationCreateRequest request) {
+    public ResponseEntity<Void> createVaccinationRecord(@Valid @RequestBody VaccinationRecordCreateRequest request) {
 
         vaccinationRecordService.createVaccinationRecord(request);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "예방접종 전체 기록 조회", description = "petId 별 모든 예방접종 기록 조회")
+    @GetMapping("/{petId}")
+    public ResponseEntity<List<VaccinationRecordResponse>> getAllVaccinationRecords(
+            @PathVariable("petId") Long petId) {
+        List<VaccinationRecordResponse> records = vaccinationRecordService.getAllVaccinationRecords(
+                petId);
 
+        return ResponseEntity.ok(records);
+    }
+
+    @Operation(summary = "예방접종 기록 상세 조회", description = "petId 별 특정 예방접종 기록 조회")
+    @GetMapping("/{petId}/detail/{id}")
+    public ResponseEntity<VaccinationRecordResponse> getVaccinationRecord(
+            @PathVariable("petId") Long petId,
+            @PathVariable("id") Long id
+    ) {
+        VaccinationRecordResponse record = vaccinationRecordService.getVaccinationRecord(
+                id, petId);
+
+        return ResponseEntity.ok(record);
+    }
 }
--- be/src/main/java/buddyguard/mybuddyguard/vaccination/service/VaccinationRecordService.java
+++ be/src/main/java/buddyguard/mybuddyguard/vaccination/service/VaccinationRecordService.java
@@ -1,9 +1,12 @@
 package buddyguard.mybuddyguard.vaccination.service;
 
-import buddyguard.mybuddyguard.vaccination.controller.request.VaccinationCreateRequest;
+import buddyguard.mybuddyguard.exception.RecordNotFoundException;
+import buddyguard.mybuddyguard.vaccination.controller.request.VaccinationRecordCreateRequest;
+import buddyguard.mybuddyguard.vaccination.controller.response.VaccinationRecordResponse;
 import buddyguard.mybuddyguard.vaccination.entity.VaccinationRecord;
 import buddyguard.mybuddyguard.vaccination.mapper.VaccinationRecordMapper;
 import buddyguard.mybuddyguard.vaccination.repository.VaccinationRecordRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,11 +20,29 @@ public class VaccinationRecordService {
     private final VaccinationRecordRepository vaccinationRecordRepository;
 
     @Transactional
-    public void createVaccinationRecord(VaccinationCreateRequest request) {
+    public void createVaccinationRecord(VaccinationRecordCreateRequest request) {
         VaccinationRecord vaccinationRecord = VaccinationRecordMapper.toEntity(request);
 
         VaccinationRecord savedVaccinationRecord = vaccinationRecordRepository.save(vaccinationRecord);
 
         log.info("SAVED VACCINATION RECORD : {}", savedVaccinationRecord);
+    }
+
+    public List<VaccinationRecordResponse> getAllVaccinationRecords(Long petId) {
+
+        List<VaccinationRecord> vaccinationRecords = vaccinationRecordRepository.findAllByPetId(petId);
+
+        if (vaccinationRecords.isEmpty()) {
+            throw new RecordNotFoundException();
+        }
+
+        return VaccinationRecordMapper.toResponseList(vaccinationRecords);
+    }
+
+    public VaccinationRecordResponse getVaccinationRecord(Long id, Long petId) {
+        VaccinationRecord vaccinationRecord = vaccinationRecordRepository.findByIdAndPetId(id, petId)
+                .orElseThrow(RecordNotFoundException::new);
+
+        return VaccinationRecordMapper.toResponse(vaccinationRecord);
     }
 }
