51c52ba21250474143679ad4f4cd7b5f6b1e0fb5
shimbaa <shim9597@gmail.com>
2024-10-21 17:17:49 +0900

    feat: 예방접종 mapper 구현 #120
    엔티티와 요청,응답 dto 를 변환해주는 유틸클래스

--- be/src/main/java/buddyguard/mybuddyguard/vaccination/controller/request/VaccinationRecordCreateRequest.java
+++ be/src/main/java/buddyguard/mybuddyguard/vaccination/controller/request/VaccinationRecordCreateRequest.java
@@ -3,7 +3,7 @@ package buddyguard.mybuddyguard.vaccination.controller.request;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-public record VaccinationCreateRequest(
+public record VaccinationRecordCreateRequest(
         @NotNull Long petId,
         @NotNull LocalDateTime vaccinationDate,
         @NotNull String vaccinationName,
--- be/src/main/java/buddyguard/mybuddyguard/vaccination/mapper/VaccinationRecordMapper.java
+++ be/src/main/java/buddyguard/mybuddyguard/vaccination/mapper/VaccinationRecordMapper.java
@@ -1,11 +1,30 @@
 package buddyguard.mybuddyguard.vaccination.mapper;
 
-import buddyguard.mybuddyguard.vaccination.controller.request.VaccinationCreateRequest;
+import buddyguard.mybuddyguard.vaccination.controller.request.VaccinationRecordCreateRequest;
+import buddyguard.mybuddyguard.vaccination.controller.response.VaccinationRecordResponse;
 import buddyguard.mybuddyguard.vaccination.entity.VaccinationRecord;
+import java.util.List;
 
 public class VaccinationRecordMapper {
 
-    public static VaccinationRecord toEntity(VaccinationCreateRequest request) {
+    public static VaccinationRecordResponse toResponse(VaccinationRecord vaccinationRecord) {
+        return new VaccinationRecordResponse(
+                vaccinationRecord.getId(),
+                vaccinationRecord.getPetId(),
+                vaccinationRecord.getVaccinationDate(),
+                vaccinationRecord.getVaccinationName(),
+                "건강",
+                vaccinationRecord.getDescription()
+        );
+    }
+
+    public static List<VaccinationRecordResponse> toResponseList(List<VaccinationRecord> vaccinationRecords) {
+        return vaccinationRecords.stream()
+                .map(VaccinationRecordMapper::toResponse)
+                .toList();
+    }
+
+    public static VaccinationRecord toEntity(VaccinationRecordCreateRequest request) {
         return VaccinationRecord.builder()
                 .petId(request.petId())
                 .vaccinationDate(request.vaccinationDate())
@@ -13,5 +32,4 @@ public class VaccinationRecordMapper {
                 .description(request.description())
                 .build();
     }
-
 }
