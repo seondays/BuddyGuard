8b800d029d466cc12a7aab2e72a6fb865cc98690
shimbaa <shim9597@gmail.com>
2024-10-21 17:02:32 +0900

    refactor: 예방접종 엔티티에 getter 추가 #120

--- be/src/main/java/buddyguard/mybuddyguard/vaccination/entity/VaccinationRecord.java
+++ be/src/main/java/buddyguard/mybuddyguard/vaccination/entity/VaccinationRecord.java
@@ -10,9 +10,11 @@ import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
+@Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
