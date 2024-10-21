7c4545198a99d2c79ca118407fc71c05fe3c6cd3
minjeongHEO <hmjoung9770@gmail.com>
2024-10-21 21:53:38 +0900

    refactor: 빌드 오류 수정

--- fe/src/components/organisms/walk/WalkModal.stories.tsx
+++ fe/src/components/organisms/walk/WalkModal.stories.tsx
@@ -1,15 +1,49 @@
 import { Meta, StoryFn } from '@storybook/react';
 
 import WalkModal, { WalkModalProps } from '@/components/organisms/walk/WalkModal';
-import { buddys } from '@/components/pages/walk/GoWalk';
 import { StyledMobileFrame } from '@/components/templates/ResponsiveLayout';
 import { BuddysType } from '@/types/map';
+import profile01 from '@public/images/profile01.png';
+import profile02 from '@public/images/profile02.png';
+import profile03 from '@public/images/profile03.png';
 
 export default {
   title: 'Organisms/WalkModal',
   component: WalkModal,
 } as Meta;
 
+const buddys = [
+  {
+    id: 1,
+    img: `${profile01}`,
+    name: '우디',
+  },
+  {
+    id: 2,
+    img: `${profile02}`,
+    name: '수잔',
+  },
+  {
+    id: 3,
+    img: `${profile03}`,
+    name: '데이',
+  },
+  {
+    id: 4,
+    img: `${profile01}`,
+    name: '진',
+  },
+  {
+    id: 5,
+    img: `${profile02}`,
+    name: '잔',
+  },
+  {
+    id: 6,
+    img: `${profile03}`,
+    name: '심바',
+  },
+];
 const Template: StoryFn<WalkModalProps> = (args) => {
   return (
     <StyledMobileFrame>
