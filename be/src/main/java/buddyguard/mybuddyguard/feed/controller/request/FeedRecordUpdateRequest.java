83004cceaa885704be868fa86aafbd413cc9bfe5
seondays <agseon9@gmail.com>
2024-10-22 14:59:15 +0900

    feat: 수정을 위한 dto 필드명 스네이크 케이스로 변경 #130

--- be/src/main/java/buddyguard/mybuddyguard/feed/controller/request/FeedRecordUpdateRequest.java
+++ be/src/main/java/buddyguard/mybuddyguard/feed/controller/request/FeedRecordUpdateRequest.java
@@ -2,13 +2,14 @@ package buddyguard.mybuddyguard.feed.controller.request;
 
 import buddyguard.mybuddyguard.feed.utils.AmountType;
 import buddyguard.mybuddyguard.feed.utils.FeedType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 
 public record FeedRecordUpdateRequest(
         LocalDateTime date,
         Long amount,
-        AmountType amountType,
-        FeedType feedType
+        @JsonProperty("amount_type") AmountType amountType,
+        @JsonProperty("feed_type") FeedType feedType
 ) {
 
 }
