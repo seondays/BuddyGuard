93c42af9126cd9f9f7985b891271e1ea94f7e4d5
minjeongHEO <hmjoung9770@gmail.com>
2024-10-24 15:51:23 +0900

    refactor: 화면 짤리는 부분 수정 #72

--- fe/src/components/pages/Notification.tsx
+++ fe/src/components/pages/Notification.tsx
@@ -1,16 +1,30 @@
+import styled from 'styled-components';
+
 import PageTitleBar from '../molecules/PageTitleBar';
 import BuddyInfoBar from '../organisms/BuddyInfoBar';
+import { NAV_HEIGHT } from '../organisms/Nav';
 import NotificationList from '../organisms/NotificationList';
 
 export default function Notification() {
   return (
-    <div style={{ padding: '1rem', height: '100vh' }}>
+    <StyledNotificationContainer>
       <PageTitleBar title="알림" />
       <BuddyInfoBar />
 
-      <div style={{ height: '70vh', overflowY: 'scroll', scrollbarWidth: 'none' }}>
+      <StyledListWrapper>
         <NotificationList />
-      </div>
-    </div>
+      </StyledListWrapper>
+    </StyledNotificationContainer>
   );
 }
+
+const StyledListWrapper = styled.div`
+  overflow-y: scroll;
+  scrollbar-width: none;
+  height: 70vh;
+  padding-bottom: ${NAV_HEIGHT};
+`;
+const StyledNotificationContainer = styled.div`
+  padding: 1rem;
+  height: 100%;
+`;
