df9dead58dd706d9ff365cd76bb4765f0e8856dc
minjeongHEO <hmjoung9770@gmail.com>
2024-10-25 20:40:18 +0900

    refactor: 마이페이지 리스트 레이아웃 수정

--- fe/src/components/molecules/MyPageItem.tsx
+++ fe/src/components/molecules/MyPageItem.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
+import { flexRow } from '@/styles/layoutStyles';
 import Paw from '@/svg/paw.svg';
 
 import Image from '../atoms/Image';
@@ -26,24 +27,27 @@ export default function MyPageItem({ title, router, onClick }: MyPageItemProps) 
     <div
       onClick={handleClick}
       style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
         borderBottom: '1px solid #ddd',
         margin: '0.1rem 0',
-        // paddingBottom: '1rem',
         cursor: 'pointer',
       }}
     >
-      <div style={{ display: 'flex', alignItems: 'center', gap: '2rem' }}>
-        <StyledIcon />
-        <StyledText>{title}</StyledText>
-      </div>
-      <Image src="/assets/icons/arrow_right.png" style={{ width: '0.5rem' }} />
+      <StyledTextWrapper>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '2rem' }}>
+          <StyledIcon />
+          <StyledText>{title}</StyledText>
+        </div>
+        <Image src="/assets/icons/arrow_right.png" style={{ width: '0.5rem' }} />
+      </StyledTextWrapper>
     </div>
   );
 }
 
+const StyledTextWrapper = styled.div`
+  padding: 0 1.5rem;
+  ${flexRow}
+  justify-content: space-between;
+`;
 const StyledIcon = styled(Paw)`
   width: 1.5rem;
   fill: ${({ theme }) => theme.currentTheme.textAccentSecondary};
