7c81faa4ba87b69a7df31bab20043f7d941d1d1d
minjeongHEO <hmjoung9770@gmail.com>
2024-10-25 20:34:23 +0900

    refactor: 마이페이지 테마에 맞게 수정 및 레이아웃 수정

--- fe/src/components/molecules/MyPageItem.tsx
+++ fe/src/components/molecules/MyPageItem.tsx
@@ -1,7 +1,9 @@
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Paw from '@/svg/paw.svg';
 
 import Image from '../atoms/Image';
-import Span from '../atoms/Span';
 
 interface MyPageItemProps {
   title: string;
@@ -28,16 +30,27 @@ export default function MyPageItem({ title, router, onClick }: MyPageItemProps) 
         alignItems: 'center',
         justifyContent: 'space-between',
         borderBottom: '1px solid #ddd',
-        margin: '2rem 0',
-        paddingBottom: '1rem',
+        margin: '0.1rem 0',
+        // paddingBottom: '1rem',
         cursor: 'pointer',
       }}
     >
       <div style={{ display: 'flex', alignItems: 'center', gap: '2rem' }}>
-        <Image src="/assets/icons/My_Buddies.png" style={{ width: '2rem' }} />
-        <Span style={{ fontSize: '1.3rem', fontWeight: 'bold' }}>{title}</Span>
+        <StyledIcon />
+        <StyledText>{title}</StyledText>
       </div>
       <Image src="/assets/icons/arrow_right.png" style={{ width: '0.5rem' }} />
     </div>
   );
 }
+
+const StyledIcon = styled(Paw)`
+  width: 1.5rem;
+  fill: ${({ theme }) => theme.currentTheme.textAccentSecondary};
+`;
+
+const StyledText = styled.span`
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: ${({ theme }) => theme.currentTheme.textPrimary};
+`;
