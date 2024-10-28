61e82179673ea0040770e4d410917d476ede5857
choejieun <wldms5221@icloud.com>
2024-10-28 23:13:15 +0900

    refactor: repository 수정 cascade 삭제 후 수동으로 저장하기 위해 repository 생성

---
+++ be/src/main/java/buddyguard/mybuddyguard/walk/repository/WalkRecordPathRepository.java
@@ -0,0 +1,8 @@
+package buddyguard.mybuddyguard.walk.repository;
+
+import buddyguard.mybuddyguard.walk.entity.WalkRecordPath;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalkRecordPathRepository extends JpaRepository<WalkRecordPath, Long> {
+
+}
