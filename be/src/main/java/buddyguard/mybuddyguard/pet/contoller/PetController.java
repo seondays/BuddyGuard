6c3ebb3dc6a644a0b7529fad57e0f2901e74cbd9
seondays <agseon9@gmail.com>
2024-10-31 22:47:19 +0900

    feat: 컨트롤러가 펫 등록 시 multipart를 받아 처리하도록 변경 #155
    펫을 등록할 때 실제로 사진과 정보를 함께 받아서 저장할 수 있도록 컨트롤러 변경

--- be/src/main/java/buddyguard/mybuddyguard/pet/contoller/PetController.java
+++ be/src/main/java/buddyguard/mybuddyguard/pet/contoller/PetController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,7 +19,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,11 +31,13 @@ public class PetController {
     private final PetService service;
 
     @Operation(summary = "펫을 등록하는 api", description = "유저에게 새로운 펫을 등록합니다")
-    @PostMapping
-    public ResponseEntity<Void> registerPet(@RequestBody PetRegisterRequest petRegisterRequest,
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> registerPet(
+            @RequestPart(name = "data") PetRegisterRequest petRegisterRequest,
+            @RequestPart(name = "image", required = false) MultipartFile imageFile,
             @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
         Long userId = customOAuth2User.getId();
-        service.register(petRegisterRequest, userId);
+        service.register(petRegisterRequest, imageFile, userId);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
