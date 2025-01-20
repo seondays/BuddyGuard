cc9c6dc87bad4f6c7b96f919282b91c92aa129f6
seondays <agseon9@gmail.com>
2025-01-20 14:11:59 +0900

    feat : 초대 링크 저장 시 RedisTemplate 사용하도록 repository 코드 변경 #2
    repository에서 RedisTemplate 사용하도록 메서드 직접 추가

--- be/src/main/java/buddyguard/mybuddyguard/invitation/repository/InvitationRepository.java
+++ be/src/main/java/buddyguard/mybuddyguard/invitation/repository/InvitationRepository.java
@@ -1,10 +1,61 @@
 package buddyguard.mybuddyguard.invitation.repository;
 
 import buddyguard.mybuddyguard.invitation.entity.InvitationInformation;
-import org.springframework.data.keyvalue.repository.KeyValueRepository;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InvitationRepository extends KeyValueRepository<InvitationInformation, String> {
-    boolean existsById(String id);
+public class InvitationRepository {
+
+    private final RedisTemplate<String, InvitationInformation> redisTemplate;
+
+    public InvitationRepository(RedisTemplate<String, InvitationInformation> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    /**
+     * 초대링크 정보를 저장합니다. 저장 시 객체의 TTL값에 따라 만료가 설정됩니다.
+     *
+     * @param invitationInformation
+     */
+    public void save(InvitationInformation invitationInformation) {
+        String key = makeKey(invitationInformation.getId());
+        redisTemplate.opsForValue()
+                .set(key, invitationInformation, invitationInformation.getTimeToLive(),
+                        TimeUnit.SECONDS);
+    }
+
+    /**
+     * uuid를 이용해 해당 초대링크 정보를 찾아서 반환합니다.
+     *
+     * @param uuid
+     * @return
+     */
+    public Optional<InvitationInformation> findById(String uuid) {
+        String key = makeKey(uuid);
+        InvitationInformation invitationInformation = redisTemplate.opsForValue()
+                .get(key);
+        return Optional.ofNullable(invitationInformation);
+    }
+
+    /**
+     * uuid를 이용해 해당 초대링크 정보를 삭제합니다.
+     *
+     * @param uuid
+     */
+    public void delete(String uuid) {
+        String key = makeKey(uuid);
+        redisTemplate.delete(key);
+    }
+
+    /**
+     * uuid 값과 키스페이스 값을 합쳐 key를 생성합니다.
+     * @param uuid
+     * @return
+     */
+    private String makeKey(String uuid) {
+        return "invitation:" + uuid;
+    }
 }
